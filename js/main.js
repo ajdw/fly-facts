904e6ca6ee8e2b06293890806f430f60249d8285
Alex Williams <alexanderjdwilliams@gmail.com>
2017-06-08 19:35:57 -0700

    added array of objects

--- js/main.js
+++ js/main.js
@@ -6,6 +6,20 @@ var facts = [
 	fact: 'the fly, example',
 	audio: 'audio/fly-fact1.mp3'
 },
-
+// 
+{
+	fact: 'the fly, example',
+	audio: 'audio/fly-fact2.mp3'
+},
+// 
+{
+	fact: 'the fly, example',
+	audio: 'audio/fly-fact3.mp3'
+},
+// 
+{
+	fact: 'the fly, example',
+	audio: 'audio/fly-fact4.mp3'
+}
 
 ]
