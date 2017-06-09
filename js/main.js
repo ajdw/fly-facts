09dc378bf9f3dfe8d4ad01253e024fcfd99b0116
Alex Williams <alexanderjdwilliams@gmail.com>
2017-06-09 14:35:49 -0700

    added onclick functioning call method

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
-alert('working');
 
+
+
+
+function getAFlyFact(){
+	alert('working');
+
+	// flyFactArray grabs our entire facts array outside of our function
+	var flyFactArray = facts;
+	var randomFact = flyFactArray[Math.floor(Math.random() * flyFactArray.length)];
+	var factText = randomFact.fact;
+
+	var factOutput = document.getElementById('fly-fact');
+	factOutput.innerHTML='<p>' + factText + '</p>';
+}
+
+
+
+
+
+// Our fly facts array
 var facts = [
 
 {
-	fact: 'the fly, example',
+	fact: 'the fly, example ipsum whatever this is ',
 	audio: 'audio/fly-fact1.mp3'
 },
 // 
