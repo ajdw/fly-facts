b466a11732776f165ed03eee3dcdf888dd68ede4
Alex Williams <alexanderjdwilliams@gmail.com>
2017-06-09 18:39:24 -0700

    added animated css styling

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 
-
-
-
 function getAFlyFact(){
-	alert('working');
 
 	// flyFactArray grabs our entire facts array outside of our function
 	var flyFactArray = facts;
@@ -12,32 +8,38 @@ function getAFlyFact(){
 
 	var factOutput = document.getElementById('fly-fact');
 	factOutput.innerHTML='<p>' + factText + '</p>';
+
+	if (factOutput.className == '') { /* for the first click, add class immediately */
+      factOutput.className = 'typewriter';
+    } else { /* for second and subequent clicks, first remove class and add after timeout */
+      factOutput.className = '';
+      window.setTimeout(function() {
+        factOutput.className = 'typewriter';
+      }, 100);
+  }
+
 }
-
-
-
-
 
 // Our fly facts array
 var facts = [
 
 {
-	fact: 'the fly, example ipsum whatever this is ',
+	fact: 'The fly thrives on eating crap all the time, which is pretty disgusting in my taste. ',
 	audio: 'audio/fly-fact1.mp3'
 },
 // 
 {
-	fact: 'the fly, example',
+	fact: 'Jeff Goldblum starred as a fly in the 1987 movie The Fly',
 	audio: 'audio/fly-fact2.mp3'
 },
 // 
 {
-	fact: 'the fly, example',
+	fact: 'The fly blah blah blah this is another fact about the fly',
 	audio: 'audio/fly-fact3.mp3'
 },
 // 
 {
-	fact: 'the fly, example',
+	fact: 'Flies vomit on their food before they eat it up',
 	audio: 'audio/fly-fact4.mp3'
 }
 
